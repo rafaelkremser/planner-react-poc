cad13ad150050aa8b0189158fead823c2784cc32
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-07-09 15:05:30 -0300

    feat: confirm trip modal

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,10 @@
-import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus } from "lucide-react"
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus, User, Mail } from "lucide-react"
 import { FormEvent, useState } from "react"
 
 export function App() {
   const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
   const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false)
+  const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false)
 
   const [emailsToInvite, setEmailsToInvite] = useState([
     'rafael@email.com.br'
@@ -19,6 +20,14 @@ export function App() {
 
   function openGuestsModal() {
     setIsGuestsModalOpen(true)
+  }
+
+  function closeConfirmTripModal() {
+    setIsConfirmTripModalOpen(false)
+  }
+
+  function openConfirmTripModal() {
+    setIsConfirmTripModalOpen(true)
   }
 
   function closeGuestsModal() {
@@ -93,14 +102,20 @@ export function App() {
             <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
               <button type="button" onClick={openGuestsModal} className="flex items-center gap-2 flex-1">
                 <UserRoundPlus className="size-5 text-zinc-400"/>
-                <span className="text-zinc-400 text-lg flex-1 text-left">Quem estará na viagem?</span>
+                {emailsToInvite.length > 0 ? (
+                  <span className="text-zinc-100 text-lg flex-1 text-left">
+                    {emailsToInvite.length} pessoa(s) convidada(s)
+                  </span>
+                ) : (
+                  <span className="text-zinc-400 text-lg flex-1 text-left">Quem estará na viagem?</span>
+                )}
               </button>
 
               <div className="w-px h-6 bg-zinc-800">
 
               </div>
 
-              <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+              <button onClick={openConfirmTripModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
                 Confirmar viagem
                 <ArrowRight className="size-5"/>
               </button>
@@ -120,8 +135,8 @@ export function App() {
             <div className="space-y-2">
               <div className="flex items-center justify-between">
                 <h2 className="text-lg font-semibold">Selecionar convidados</h2>
-                <button type="button" onClick={closeGuestsModal}>
-                  <X className="size-5 text-zinc-400" />
+                <button type="button">
+                  <X onClick={closeGuestsModal} className="size-5 text-zinc-400" />
                 </button>
               </div>
               <p className="text-sm text-zinc-400">
@@ -134,8 +149,8 @@ export function App() {
                 return (
                   <div key={email} className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
                     <span className="text-zinc-300">{email}</span>
-                    <button type="button" onClick={() => removeEmailFromInvites(email)}>
-                      <X className="size-4 text-zinc-400" />
+                    <button type="button">
+                      <X onClick={() => removeEmailFromInvites(email)} className="size-4 text-zinc-400" />
                     </button>
                   </div>
                 )
@@ -153,6 +168,49 @@ export function App() {
               <button type="submit" className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
                 Convidar
                 <Plus className="size-5"/>
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+
+      {isConfirmTripModalOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">Confirmar criação de viagem</h2>
+                <button type="button">
+                  <X onClick={closeConfirmTripModal} className="size-5 text-zinc-400" />
+                </button>
+              </div>
+              <p className="text-sm text-zinc-400">
+                Para concluir a criação da viagem para <span className="font-semibold text-zinc-100">Florianópolis, Brasil</span> nas datas de <span className="font-semibold text-zinc-100">16 a 27 de Agosto de 2024</span> preencha seus dados abaixo:
+              </p>
+            </div>
+
+            <form onSubmit={addNewEmailToInvite} className="space-y-3">
+              <div className="h-14 px-4 flex items-center gap-2 bg-zinc-950 border border-zinc-800 rounded-lg">
+                <User className="size-5 text-zinc-400" />
+                <input
+                  name="name"
+                  placeholder="Seu nome completo"
+                  className="bg-transparent text-base placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+
+              <div className="h-14 px-4 flex items-center gap-2 bg-zinc-950 border border-zinc-800 rounded-lg">
+                <Mail className="size-5 text-zinc-400" />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Seu e-mail pessoal"
+                  className="bg-transparent text-base placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+
+              <button type="submit" className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400">
+                Confirmar criação da viagem
               </button>
             </form>
           </div>
